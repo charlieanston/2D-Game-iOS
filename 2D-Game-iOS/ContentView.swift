88c9234841af8860b3972cdd1f62a0441c04e5c4
charlieanston <s3805504@rmit.edu.vn>
2022-08-26 12:35:58 +0700

    Initial Commit

---
+++ 2D-Game-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  2D-Game-iOS
+//
+//  Created by Hung Le Tran Trong on 26/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 2D-Game-iOS/_D_Game_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  _D_Game_iOSApp.swift
+//  2D-Game-iOS
+//
+//  Created by Hung Le Tran Trong on 26/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct _D_Game_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
