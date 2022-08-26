72d01a423b320436ea862b05ed7ed078dec528a3
charlieanston <s3805504@rmit.edu.vn>
2022-08-26 13:32:35 +0700

    stuff

---
+++ 2D-Game-iOS/Model/ColorConstants.swift
@@ -0,0 +1,17 @@
+//
+//  ColorConstants.swift
+//  2D-Game-iOS
+//
+//  Created by Hung Le Tran Trong on 26/08/2022.
+//
+
+import Foundation
+import SwiftUI
+
+struct ColorConstants{
+    static let ceriseRed = Color(red: 0.85, green: 0.16, blue: 0.39)
+    static let froly = Color(red: 0.93, green: 0.38, blue: 0.57)
+    static let wewak = Color(red: 0.95, green: 0.65, blue: 0.75)
+    static let morningGlory = Color(red: 0.60, green: 0.88, blue: 0.89)
+    static let easternBlue = Color(red: 0.09, green: 0.65, blue: 0.64)
+}
---
+++ 2D-Game-iOS/Views/Welcome/WelcomeView.swift
@@ -0,0 +1,28 @@
+//
+//  WelcomeView.swift
+//  GameCompaniesList
+//
+//  Created by Hung Le Tran Trong on 26/08/2022.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    @State var isWelcomeActive: Bool = true
+    var body: some View {
+        ZStack {
+            if isWelcomeActive {
+                GreetingView(active: $isWelcomeActive)
+            }
+//            else {
+//                CompanyList()
+//            }
+        }
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
